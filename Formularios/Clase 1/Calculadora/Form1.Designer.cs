c04eb0d79d1ea968d0049b661b0a275cb0e626be
Axel Reyes Prado <axelgrepra@gmail.com>
2025-10-09 15:30:52 -0300

    Formularios

---
+++ Formularios/Clase 1/Calculadora/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSuma = new Button();
+            btnRestar = new Button();
+            btnMultiplicar = new Button();
+            btnDividir = new Button();
+            txt1 = new TextBox();
+            txt2 = new TextBox();
+            label1 = new Label();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            btnSuma.Location = new Point(97, 250);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(75, 23);
+            btnSuma.TabIndex = 0;
+            btnSuma.Text = "Sumar";
+            btnSuma.UseVisualStyleBackColor = true;
+            btnSuma.Click += button1_Click;
+            // 
+            // btnRestar
+            // 
+            btnRestar.Location = new Point(268, 250);
+            btnRestar.Name = "btnRestar";
+            btnRestar.Size = new Size(75, 23);
+            btnRestar.TabIndex = 1;
+            btnRestar.Text = "Restar";
+            btnRestar.UseVisualStyleBackColor = true;
+            btnRestar.Click += btnRestar_Click;
+            // 
+            // btnMultiplicar
+            // 
+            btnMultiplicar.Location = new Point(443, 250);
+            btnMultiplicar.Name = "btnMultiplicar";
+            btnMultiplicar.Size = new Size(75, 23);
+            btnMultiplicar.TabIndex = 2;
+            btnMultiplicar.Text = "Multiplicar";
+            btnMultiplicar.UseVisualStyleBackColor = true;
+            btnMultiplicar.Click += btnMultiplicar_Click;
+            // 
+            // btnDividir
+            // 
+            btnDividir.Location = new Point(572, 250);
+            btnDividir.Name = "btnDividir";
+            btnDividir.Size = new Size(75, 23);
+            btnDividir.TabIndex = 3;
+            btnDividir.Text = "Dividir";
+            btnDividir.UseVisualStyleBackColor = true;
+            btnDividir.Click += btnDividir_Click;
+            // 
+            // txt1
+            // 
+            txt1.Location = new Point(205, 101);
+            txt1.Name = "txt1";
+            txt1.Size = new Size(100, 23);
+            txt1.TabIndex = 4;
+            txt1.TextChanged += txt1_TextChanged;
+            // 
+            // txt2
+            // 
+            txt2.Location = new Point(443, 101);
+            txt2.Name = "txt2";
+            txt2.Size = new Size(100, 23);
+            txt2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(356, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(15, 15);
+            label1.TabIndex = 6;
+            label1.Text = "+";
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(356, 171);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(59, 15);
+            lblResultado.TabIndex = 7;
+            lblResultado.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(label1);
+            Controls.Add(txt2);
+            Controls.Add(txt1);
+            Controls.Add(btnDividir);
+            Controls.Add(btnMultiplicar);
+            Controls.Add(btnRestar);
+            Controls.Add(btnSuma);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSuma;
+        private Button btnRestar;
+        private Button btnMultiplicar;
+        private Button btnDividir;
+        private TextBox txt1;
+        private TextBox txt2;
+        private Label label1;
+        private Label lblResultado;
+    }
+}
---
+++ Formularios/Clase 1/Calculadora/Form1.cs
@@ -0,0 +1,56 @@
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(txt1.Text);
+            int valor2 = int.Parse(txt2.Text);
+            int resultado = valor1 + valor2;
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(txt1.Text);
+            int valor2 = int.Parse(txt2.Text);
+            int resultado = valor1 - valor2;
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(txt1.Text);
+            int valor2 = int.Parse(txt2.Text);
+            int resultado = valor1 * valor2;
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(txt1.Text);
+            int valor2 = int.Parse(txt2.Text);
+            if (valor2 == 0)
+            {
+                MessageBox.Show("No se puede dividir entre cero");
+                return;
+            }
+            else
+            {
+                int resultado = valor1 / valor2;
+                lblResultado.Text = resultado.ToString();
+
+            }
+        }
+    }
+}
